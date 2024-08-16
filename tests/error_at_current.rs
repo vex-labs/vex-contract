67d3fb10b9912200741a103c69a8881893305e27
PiVortex <owenhassall@hotmail.co.uk>
2024-08-16 18:29:20 +0100

    start tests

---
+++ tests/error_at_current.rs
@@ -0,0 +1,25 @@
+use near_workspaces::result::ExecutionFinalResult;
+use near_workspaces::{Account, Contract};
+use near_sdk::NearToken;
+use near_sdk::json_types::U128;
+
+#[tokio::test]
+
+async fn test_contract_is_operational() -> Result<(), Box<dyn std::error::Error>> {
+
+    // Setup, contracts, accounts, FT, register, send to users initally, etc
+
+    // 2 users 
+
+
+    // New match is created
+
+    // Bets are placed 
+
+    // end betting
+
+    // cancel match
+
+
+    Ok(())
+}
