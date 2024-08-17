579c3670157048183ad7889b92d0632d37452449
PiVortex <owenhassall@hotmail.co.uk>
2024-08-17 17:54:03 +0100

    tests in progress

--- tests/setup.rs
+++ tests/setup.rs
@@ -1,44 +1,165 @@
-// This file contains the setup for all tests 
+// This file contains the setup for all tests
 
+use near_sdk::json_types::U128;
+use near_sdk::{AccountId, Gas, NearToken};
+use near_workspaces::error::Error;
 use near_workspaces::network::Sandbox;
 use near_workspaces::result::ExecutionFinalResult;
-use near_workspaces::{Account, Contract, Worker, Result};
-use near_workspaces::error::Error;
-use near_sdk::NearToken;
-use near_sdk::json_types::U128;
+use near_workspaces::{Account, Contract, Result, Worker};
+use serde_json::json;
 
-const TEN_NEAR: NearToken = NearToken::from_near(10);
-
+const FIFTY_NEAR: NearToken = NearToken::from_near(50);
+const FT_WASM_FILEPATH: &str = "./tests/fungible_token.wasm";
+pub const ONE_USDC: u128 = 1_000_000_000_000_000_000_000_000;
 
 pub struct TestSetup {
     pub sandbox: Worker<Sandbox>,
     pub alice: Account,
     pub bob: Account,
+    pub admin: Account,
+    pub vex_contract: Contract,
+    pub usdc_contract: Contract,
 }
 
 impl TestSetup {
-    pub async fn new() -> Result<Self, Error> {
+    pub async fn new() -> Result<Self, Box<dyn std::error::Error>> {
+        // Create sandbox
         let sandbox = near_workspaces::sandbox().await?;
 
+        // Create accounts
         let root: near_workspaces::Account = sandbox.root_account()?;
 
         let alice = create_account(&root, "alice").await?;
         let bob = create_account(&root, "bob").await?;
+        let admin = create_account(&root, "admin").await?;
 
-        Ok(TestSetup { sandbox, alice, bob })
+        // Deploy VEX contract
+        // let contract_wasm = near_workspaces::compile_project("./").await?;
+        let contract_wasm = std::fs::read("./target/wasm32-unknown-unknown/release/vex_contracts.wasm")?;
+        let vex_contract = sandbox.dev_deploy(&contract_wasm).await?;
+
+        // Deploy FT contract
+        let ft_wasm = std::fs::read(FT_WASM_FILEPATH)?;
+        let usdc_contract = sandbox.dev_deploy(&ft_wasm).await?;
+
+        // Initialize USDC FT contract
+        let res = usdc_contract
+            .call("new_default_meta")
+            .args_json(serde_json::json!({
+            "owner_id": admin.id(),
+            "total_supply": U128(1_000_000_000 * ONE_USDC), // 1 billion USDC
+            }))
+            .transact()
+            .await?;
+
+        assert!(res.is_success(), "Failed to initialize USDC FT contract");
+
+        // Register accounts in FT contract and send 100 FTs
+        for account in [
+            alice.clone(),
+            bob.clone(),
+            vex_contract.as_account().clone(),
+        ]
+        .iter()
+        {
+            let register = account
+                .call(usdc_contract.id(), "storage_deposit")
+                .args_json(serde_json::json!({ "account_id": account.id() }))
+                .deposit(NearToken::from_millinear(8))
+                .transact()
+                .await?;
+
+            assert!(register.is_success(), "Failed to register account in USDC FT contract");
+
+            // Transfer 100 FTs to accounts
+            let transfer = ft_transfer(
+                &admin,
+                account.clone(),
+                usdc_contract.clone(),
+                U128(100 * ONE_USDC),
+            )
+            .await?;
+            assert!(transfer.is_success(), "Failed to transfer 100 FTs to account");
+        }
+
+        // Initialize VEX contract
+        let init: ExecutionFinalResult = vex_contract
+            .call("init")
+            .args_json(
+                json!({"admin": admin.id(), "usdc_contract": usdc_contract.as_account().id()}),
+            )
+            .transact()
+            .await?;
+
+        assert!(init.is_success(), "Failed to initialize VEX contract");
+
+        Ok(TestSetup {
+            sandbox,
+            alice,
+            bob,
+            admin,
+            vex_contract,
+            usdc_contract,
+        })
     }
 }
 
-async fn create_account(
-    root: &near_workspaces::Account,
-    name: &str,
-) -> Result<Account, Error> {
+async fn create_account(root: &near_workspaces::Account, name: &str) -> Result<Account, Error> {
     let subaccount = root
         .create_subaccount(name)
-        .initial_balance(TEN_NEAR)
+        .initial_balance(FIFTY_NEAR)
         .transact()
         .await?
         .unwrap();
 
     Ok(subaccount)
+}
+
+async fn ft_transfer(
+    root: &near_workspaces::Account,
+    account: Account,
+    usdc_contract: Contract,
+    transfer_amount: U128,
+) -> Result<ExecutionFinalResult, Box<dyn std::error::Error>> {
+    let transfer = root
+        .call(usdc_contract.id(), "ft_transfer")
+        .args_json(serde_json::json!({
+            "receiver_id": account.id(),
+            "amount": transfer_amount
+        }))
+        .deposit(NearToken::from_yoctonear(1))
+        .transact()
+        .await?;
+    Ok(transfer)
+}
+
+pub async fn ft_balance_of(
+    usdc_contract: &Contract,
+    account_id: &AccountId,
+) -> Result<U128, Box<dyn std::error::Error>> {
+    let result = usdc_contract
+        .view("ft_balance_of")
+        .args_json(json!({"account_id": account_id}))
+        .await?
+        .json()?;
+
+    Ok(result)
+}
+
+pub async fn ft_transfer_call(
+    account: Account,
+    usdc_contract_id: &AccountId,
+    receiver_id: &AccountId,
+    amount: U128,
+    msg: String,
+) -> Result<ExecutionFinalResult, Box<dyn std::error::Error>> {
+    let transfer = account
+        .call(usdc_contract_id, "ft_transfer_call")
+        .args_json(serde_json::json!({"receiver_id": receiver_id, "amount": amount, "msg": msg }))
+        .deposit(NearToken::from_yoctonear(1))
+        .gas(Gas::from_tgas(50))
+        .transact()
+        .await?;
+
+    Ok(transfer)
 }
--- tests/usual_flow.rs
+++ tests/usual_flow.rs
@@ -1,39 +1,138 @@
-use near_workspaces::result::ExecutionFinalResult;
-use near_workspaces::{Account, Contract};
-use near_sdk::NearToken;
-use near_sdk::json_types::U128;
+use near_sdk::json_types::{U128, U64};
+use vex_contracts::Team;
+mod setup;
+use crate::setup::*;
 
 #[tokio::test]
 
-async fn test_contract_is_operational() -> Result<(), Box<dyn std::error::Error>> {
+async fn test_usual_flow() -> Result<(), Box<dyn std::error::Error>> {
+    let TestSetup {
+        sandbox,
+        alice,
+        bob,
+        admin,
+        vex_contract,
+        usdc_contract,
+    } = setup::TestSetup::new().await?;
 
-    // Setup, contracts, accounts, FT, register, send to users initally, etc
+    // Create a new match
+    let create_match = admin
+        .call(vex_contract.id(), "create_match")
+        .args_json(serde_json::json!({"game": "CSGO", "team_1": "RUBY", "team_2": "Nexus", "in_odds_1": 1.2, "in_odds_2": 1.6, "date": "17/08/2024"}))
+        .transact()
+        .await?;
 
-    // 2 users 
+    assert!(create_match.is_success());
 
+    // Alice places a bet of 10 USDC on the winning team
+    let mut alice_bet = ft_transfer_call(
+        alice.clone(),
+        usdc_contract.id(),
+        vex_contract.id(),
+        U128(10 * ONE_USDC),
+        serde_json::json!({"match_id": "RUBY-Nexus-17/08/2024", "team": Team::Team1}).to_string(),
+    )
+    .await?;
 
-    // New match is created
+    assert!(alice_bet.is_success(), "ft_transfer_call failed on Alice's first bet");
 
-    // Bets are placed 
+    let mut vex_contract_balance: U128 = ft_balance_of(&usdc_contract, vex_contract.id()).await?;
+    assert_eq!(vex_contract_balance, U128(110 * ONE_USDC), "Vex contract balance is not correct after Alice's first bet");
+    let mut alice_balance: U128 = ft_balance_of(&usdc_contract, alice.id()).await?;
+    assert_eq!(alice_balance, U128(90 * ONE_USDC), "Alice's balance is not correct after her first bet");
 
-    // False bets are placed
+    let mut bet = vex_contract.view("get_bet").args_json(serde_json::json!({"bettor": alice.id(), "bet_id": U64(1)})).await;
+    assert!(bet.is_ok(), "Failed to get Alice's bet");
+
+    // Bob places a bet of 5 USDC on the losing team
+    let mut bob_bet = ft_transfer_call(
+        bob.clone(),
+        usdc_contract.id(),
+        vex_contract.id(),
+        U128(5 * ONE_USDC),
+        serde_json::json!({"match_id": "RUBY-Nexus-17/08/2024", "team": Team::Team2}).to_string(),
+    )
+    .await?;
+
+    assert!(bob_bet.is_success(), "ft_transfer_call failed on Bob's first bet");
+
+    vex_contract_balance = ft_balance_of(&usdc_contract, vex_contract.id()).await?;
+    assert_eq!(vex_contract_balance, U128(115 * ONE_USDC), "Vex contract balance is not correct after Bob's first bet");
+    let mut bob_balance: U128 = ft_balance_of(&usdc_contract, bob.id()).await?;
+    assert_eq!(bob_balance, U128(95 * ONE_USDC), "Bob's balance is not correct after his first bet");
+
+    bet = vex_contract.view("get_bet").args_json(serde_json::json!({"bettor": bob.id(), "bet_id": U64(2)})).await;
+    assert!(bet.is_ok(), "Failed to get Bob's first bet");
+    
+    // Bob places a bet of 2 USDC on the winning team
+    bob_bet = ft_transfer_call(
+        bob.clone(),
+        usdc_contract.id(),
+        vex_contract.id(),
+        U128(2 * ONE_USDC),
+        serde_json::json!({"match_id": "RUBY-Nexus-17/08/2024", "team": Team::Team1}).to_string(),
+    )
+    .await?;
+
+    assert!(bob_bet.is_success(), "ft_transfer_call failed on Bob's second bet");
+
+    vex_contract_balance = ft_balance_of(&usdc_contract, vex_contract.id()).await?;
+    assert_eq!(vex_contract_balance, U128(117 * ONE_USDC), "Vex contract balance is not correct after Bob's second bet");
+    bob_balance = ft_balance_of(&usdc_contract, bob.id()).await?;
+    assert_eq!(bob_balance, U128(93 * ONE_USDC), "Bob's balance is not correct after his second bet");
+    
+    // Alice places a bet on an invalid match
+    alice_bet = ft_transfer_call(
+        alice.clone(),
+        usdc_contract.id(),
+        vex_contract.id(),
+        U128(10 * ONE_USDC),
+        serde_json::json!({"match_id": "Furia-Nexus-17/08/2024", "team": Team::Team1}).to_string(),
+    )
+    .await?;
+
+    assert!(alice_bet.is_success(), "ft_transfer_call failed on Alice's invalid match bet");
+
+    vex_contract_balance = ft_balance_of(&usdc_contract, vex_contract.id()).await?;
+    assert_eq!(vex_contract_balance, U128(117 * ONE_USDC), "Vex contract balance is not correct after Alice's invalid match bet");
+    alice_balance = ft_balance_of(&usdc_contract, alice.id()).await?;
+    assert_eq!(alice_balance, U128(90 * ONE_USDC), "Alice's balance is not correct after her invalid match bet");
+
+    bet = vex_contract.view("get_bet").args_json(serde_json::json!({"bettor": alice.id(), "bet_id": U64(3)})).await;
+    assert!(bet.is_err(), "Wrongly managed to get Alice's invalid match bet");
 
     // end betting 
+    
 
     // users try to place bets
 
     // finish match
 
-    // users try to place bets 
+    // users try to place bets
 
     // users try to claim funds for each others
 
     // user claims funds
 
-    // user tries to claim funds again 
+    // user tries to claim funds again
 
-    // 
-
+    //
 
     Ok(())
 }
+
+
+
+
+
+// for error checking 
+
+// if alice_bet.is_success() {
+//     println!("Bet was successful!");
+// } else {
+//     // Directly iterate over the failures vector
+//     for failure in alice_bet.failures() {
+//         println!("Failure: {:?}", failure);
+//     }
+//     panic!("Alice's bet failed.");
+// }
