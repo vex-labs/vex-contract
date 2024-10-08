1344747ddf9817025264e45f74a97bb768f44fb1
PiVortex <owenhassall@hotmail.co.uk>
2024-10-08 18:32:45 +0700

    WIP

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::VecDeque;
+
 use near_sdk::json_types::{U128, U64};
 use near_sdk::store::{IterableMap, LookupMap};
 use near_sdk::{near, AccountId, PanicOnDefault};
@@ -20,11 +22,15 @@ pub struct Contract {
     pub vex_token_contract: AccountId,
     pub fees_fund: U128,
     pub insurance_fund: U128,
+    pub funds_to_payout: U128,
     pub users_stake: LookupMap<AccountId, UserStake>,
     pub total_staked_balance: U128,
     pub total_stake_shares: U128,
     pub treasury: AccountId,
-    pub pool_id: u64,
+    pub ref_pool_id: u64,
+    pub usdc_staking_rewards: U128,
+    pub staking_rewards_queue: VecDeque<MatchStakeInfo>,
+    pub last_stake_swap_timestamp: U64,
 }
 
 #[near(serializers = [borsh])]
@@ -90,6 +96,12 @@ impl Default for UserStake {
     }
 }
 
+#[near(serializers = [json, borsh])]
+pub struct MatchStakeInfo {
+    pub staking_rewards: U128,
+    pub stake_end_time: U64,
+}
+
 construct_uint! {
     /// 256-bit unsigned integer.
     pub struct U256(4);
@@ -103,6 +115,8 @@ pub const ONE_USDC: U128 = U128(1_000_000); // Note that this will have to chang
 pub const FIFTY_VEX: U128 = U128(50_000_000_000_000_000_000); // Note that this will have to change if VEX decimals are not 18
 pub const STAKE_SHARE_PRICE_GUARANTEE_FUND: u128 = 1_000_000_000_000_000;
 pub const INITIAL_ACCOUNT_BALANCE: u128 = 50_000_000_000_000_000_000; // The contract needs to be initialized with 50 VEX
+pub const ONE_MONTH: u64 = 2_628_000_000_000_000;
+pub const ONE_WEEK: u64 = 604_800_000_000_000;
 
 #[near]
 impl Contract {
@@ -113,7 +127,7 @@ impl Contract {
         usdc_contract: AccountId,
         vex_token_contract: AccountId,
         treasury: AccountId,
-        pool_id: u64,
+        ref_pool_id: u64,
     ) -> Self {
         let total_staked_balance = U128(INITIAL_ACCOUNT_BALANCE - STAKE_SHARE_PRICE_GUARANTEE_FUND);
 
@@ -126,11 +140,15 @@ impl Contract {
             vex_token_contract,
             fees_fund: U128(0),
             insurance_fund: U128(0),
+            funds_to_payout: U128(0),
             users_stake: LookupMap::new(b"s"),
             total_staked_balance,
             total_stake_shares: total_staked_balance,
             treasury,
-            pool_id,
+            ref_pool_id,
+            usdc_staking_rewards: U128(0),
+            staking_rewards_queue: VecDeque::new(),
+            last_stake_swap_timestamp: U64(0),
         }
     }
 }
