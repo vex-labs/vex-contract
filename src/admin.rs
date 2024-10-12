12c66d37a803f2a15cdefda98ca218997e9b1695
PiVortex <owenhassall@hotmail.co.uk>
2024-10-12 17:15:34 +0700

    add some comments

--- src/admin.rs
+++ src/admin.rs
@@ -5,6 +5,7 @@ use crate::*;
 
 #[near]
 impl Contract {
+    // Changes the admin of the contract
     pub fn change_admin(&mut self, new_admin: AccountId) {
         require!(
             env::predecessor_account_id() == self.admin,
@@ -14,6 +15,7 @@ impl Contract {
         self.admin = new_admin;
     }
 
+    // Creates a new match
     pub fn create_match(
         &mut self,
         game: String,
@@ -30,6 +32,7 @@ impl Contract {
 
         let match_id: MatchId = format!("{}-{}-{}", team_1, team_2, date);
 
+        // Calculate inital pool sizes
         let in_prob_1: f64 = 1.0 / in_odds_1;
         let in_prob_2: f64 = 1.0 / in_odds_2;
         let divider = in_prob_1 + in_prob_2;
@@ -57,9 +60,11 @@ impl Contract {
             winner,
         };
 
+        // Insert new match
         self.matches.insert(match_id, new_match);
     }
 
+    // When a match starts
     pub fn end_betting(&mut self, match_id: &MatchId) {
         require!(
             env::predecessor_account_id() == self.admin,
@@ -79,6 +84,7 @@ impl Contract {
         relevant_match.match_state = MatchState::Current;
     }
 
+    // When a match finishes
     pub fn finish_match(&mut self, match_id: &MatchId, winner: Team) {
         require!(
             env::predecessor_account_id() == self.admin,
@@ -102,6 +108,8 @@ impl Contract {
             - relevant_match.team_1_initial_pool.0
             - relevant_match.team_2_initial_pool.0;
 
+        // Calculate the difference between the total bets and the potential winnings
+        // and whether it is a profit or loss
         let (difference, is_profit) = match winner {
             Team::Team1 => {
                 self.funds_to_payout =
@@ -135,12 +143,14 @@ impl Contract {
             }
         };
 
+        // Send to relevant function to handle profit or loss scenario
         match is_profit {
             true => self.handle_profit(difference),
             false => self.handle_loss(difference),
         };
     }
 
+    // Cancels a match and puts it in an error state
     pub fn cancel_match(&mut self, match_id: &MatchId) {
         require!(
             env::predecessor_account_id() == self.admin,
@@ -163,6 +173,7 @@ impl Contract {
         relevant_match.match_state = MatchState::Error;
     }
 
+    // Removes an amount of USDC from the fees fund and sends it to the receiver
     pub fn take_from_fees_fund(&mut self, amount: U128, receiver: AccountId) -> U128 {
         require!(
             env::predecessor_account_id() == self.admin,
@@ -181,9 +192,11 @@ impl Contract {
 
         self.fees_fund = U128(self.fees_fund.0 - amount.0);
 
+        // Returns how much is left in the fees fund
         self.fees_fund
     }
 
+    // Removes an amount of USDC from the insurance fund and sends it to the receiver
     pub fn take_from_insurance_fund(&mut self, amount: U128, receiver: AccountId) -> U128 {
         require!(
             env::predecessor_account_id() == self.admin,
@@ -202,16 +215,18 @@ impl Contract {
 
         self.insurance_fund = U128(self.insurance_fund.0 - amount.0);
 
+        // Returns how much is left in the insurance fund
         self.insurance_fund
     }
 
+    // Handles the case when a match finishes and there is a profit
     pub(crate) fn handle_profit(&mut self, profit: u128) {
         // Calculate how profit is distributed
-        let usdc_staking_rewards =
+        let usdc_for_staking =
             (U256::from(60) * U256::from(profit) / U256::from(100)).as_u128();
         let treasury_rewards = (U256::from(30) * U256::from(profit) / U256::from(100)).as_u128();
         let insurace_rewards = (U256::from(5) * U256::from(profit) / U256::from(100)).as_u128();
-        let fees_rewards = profit - usdc_staking_rewards - treasury_rewards - insurace_rewards;
+        let fees_rewards = profit - usdc_for_staking - treasury_rewards - insurace_rewards;
 
         // Increase fees and insurance funds
         self.fees_fund = U128(self.fees_fund.0 + fees_rewards);
@@ -227,22 +242,25 @@ impl Contract {
         // match being added to the list so extra rewards are not distributed
         self.perform_stake_swap();
 
-        self.usdc_staking_rewards = U128(self.usdc_staking_rewards.0 + usdc_staking_rewards);
+        self.usdc_staking_rewards = U128(self.usdc_staking_rewards.0 + usdc_for_staking);
 
         // Add to staking rewards queue
         let match_stake_info = MatchStakeInfo {
-            staking_rewards: U128(usdc_staking_rewards),
+            staking_rewards: U128(usdc_for_staking),
             stake_end_time: U64(env::block_timestamp() + ONE_MONTH),
         };
         self.staking_rewards_queue.push_back(match_stake_info);
     }
 
+    // Handles the case when a match finishes and there is a loss
     pub(crate) fn handle_loss(&mut self, loss: u128) {
+        // If the loss can be covered by the insurance fund then do so
         if loss < self.insurance_fund.0 {
             self.insurance_fund = U128(self.insurance_fund.0 - loss);
-            return
+            return;
         }
 
+        // Calculate how much more USDC is needed to cover the loss
         let difference = loss - self.insurance_fund.0;
 
         // Cross contract call to check how much vex is needed to be sold to cover the difference + 10%
@@ -250,10 +268,8 @@ impl Contract {
 
         // swap this amount of vex to usdc
 
-        // Check that the amount of returned usdc + insurance fund is enough to cover the loss 
+        // Check that the amount of returned usdc + insurance fund is enough to cover the loss
         // If not emit some log that we gotta add money to the contract (this should hopefully never happen), have a variable to keep track of amount owed
         // add left over usdc to insurance fund
-
-
     }
 }
