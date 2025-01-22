295fb123dd99daa675dd9c84aaf5c2fb26dcac03
Volodymyr Matseliukh <volodymyr.matselyukh@gmail.com>
2025-01-22 17:26:24 +0100

    pr changes

--- src/betting/bettor_tests.rs
+++ src/betting/bettor_tests.rs
@@ -5,15 +5,14 @@ use crate::{betting::bettor::determine_potential_winnings, Team};
 #[test]
 fn test_determine_potential_winnings() {
     determine_potential_winnings_base(
-        500,
-        1000, 
-        100, 
-        268);
+        500_000_000,
+        1000_000_000, 
+        100_000_000, 
+        268_877_673);
 }
 
 
 fn determine_potential_winnings_base(team_1_total_bets: u128, team_2_total_bets: u128, bet_amount: u128, expected_winnings: u128) {
-    // Arrange
     let team_1 = Team::Team1;
     
     let team_1_total_bets = U128(team_1_total_bets);
@@ -23,10 +22,8 @@ fn determine_potential_winnings_base(team_1_total_bets: u128, team_2_total_bets:
 
     let expected_potential_winnings = U128(expected_winnings);
 
-    // Act
     let actual_potential_winnings = determine_potential_winnings(&team_1, &team_1_total_bets, &team_2_total_bets, &bet_amount);
 
-    // Assert
     assert!(
         expected_potential_winnings == actual_potential_winnings,
         "Winnings calculation error. Actual: {} Expected: {}", 
--- src/unit_tests/mod.rs
+++
@@ -1,2 +0,0 @@
-pub mod betting;
-pub mod setup;
